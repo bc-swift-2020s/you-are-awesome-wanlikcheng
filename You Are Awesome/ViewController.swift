ccca01884ff9f1f1c9d0a5d1575b5d4880d80ce0
kelvin <chengwj@bc.edu>
2020-01-20 20:13:20 -0500

    Completed 2.9 More Messages + Better Code: Using Arrays and Working with Xcode Playgrounds

--- You Are Awesome/ViewController.swift
+++ You Are Awesome/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
     
     var imageNumber = 0
+    var messageNumber = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +22,12 @@ class ViewController: UIViewController {
     }
 
     @IBAction func showMessageButton(_ sender: UIButton) {
+        let messages = ["You are awesome!", "You are great!", "You are fantastic!", "You are fabulous!"]
+        messageLabel.text = messages[messageNumber]
+        messageNumber += 1
+        if messageNumber == messages.count {
+            messageNumber = 0
+        }
         
         print(imageNumber)
         //let imageName = "image" + String(imageNumber)
